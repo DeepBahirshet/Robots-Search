d78a002a15401163a4383d2ef61ede6252231365
Deepsb <deepbahirshet@gmail.com>
2020-09-09 14:06:36 +0530

    Updates

---
+++ precache-manifest.17c245d03a816de1c888e62a2bac32c8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68fc833748e0c64617863f18306a6dc0",
+    "url": "/Robots-Search/index.html"
+  },
+  {
+    "revision": "21a06b2c159dcb45e3d0",
+    "url": "/Robots-Search/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "78ee9de4e832ffafaed5",
+    "url": "/Robots-Search/static/css/main.94bc0952.chunk.css"
+  },
+  {
+    "revision": "21a06b2c159dcb45e3d0",
+    "url": "/Robots-Search/static/js/2.fdba7dc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robots-Search/static/js/2.fdba7dc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78ee9de4e832ffafaed5",
+    "url": "/Robots-Search/static/js/main.8a2f3dd9.chunk.js"
+  },
+  {
+    "revision": "224f91433bd2831b8ba6",
+    "url": "/Robots-Search/static/js/runtime-main.bcd85eb9.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robots-Search/static/media/SEGA.12a3db8b.woff"
+  }
+]);
